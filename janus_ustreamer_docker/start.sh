8f33e15fca19408281676afe122e1c2a26bc12b0
André Esser <bepandre@hotmail.com>
2022-02-02 19:33:36 +0000

    adds the remaining flags from the original tinypilot original

--- janus_ustreamer_docker/start.sh
+++ janus_ustreamer_docker/start.sh
@@ -1,2 +1,2 @@
-/ustreamer/ustreamer -f 30 --host 127.0.0.1 -p8001 --h264-sink tinypilot::ustreamer::h264 --h264-sink-rm --h264-sink-mode 777 --format uyvy --encoder omx &
+/ustreamer/ustreamer -f 30 --host 127.0.0.1 -p8001 --h264-sink tinypilot::ustreamer::h264 --h264-sink-rm --h264-sink-mode 777 --format uyvy --encoder omx --persistent --dv-timings --workers 3 --drop-same-frames 30 &
 /opt/janus/bin/janus -F /opt/janus/lib/janus/configs/ -P /opt/janus/lib/janus/plugins/
